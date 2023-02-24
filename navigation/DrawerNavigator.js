eefdadf41626c1b2b78d5e270046105ef55f6a56
xdKya <90745676+xdKya@users.noreply.github.com>
2023-02-24 18:58:30 -0300

    Add files via upload

---
+++ navigation/DrawerNavigator.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import BottomTabNavigator from "./TabNavigator";
+import Profile from "../screens/Profile";
+
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => {
+  return (
+    <Drawer.Navigator
+    //screenOptions={{ headerShown: false }}
+    >
+      <Drawer.Screen name="Home" component={BottomTabNavigator} />
+      <Drawer.Screen name="Profile" component={Profile} />
+    </Drawer.Navigator>
+  );
+};
+
+export default DrawerNavigator;
